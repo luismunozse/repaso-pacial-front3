b4e8e646b98769578f6d312adff4daaf1bdd6a1a
Luis Muñoz <luismunozse@gmail.com>
2023-08-14 23:56:07 -0300

    repaso-parcial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Carga de estudiantes</h1>
+        <form></form>
+        <Card/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Hola soy una card</div>
+  )
+}
+
+export default Card
